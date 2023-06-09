5873190dda001a163ef96eb38d1753e64b4730a2
prem <prakash.prem45@gmail.com>
2023-06-09 16:11:33 +0530

    Update stop-ec2.sh

--- stop-ec2.sh
+++ stop-ec2.sh
@@ -1,4 +1,5 @@
 echo "  --> Run Command in EC2: docker-compose -f docker-compose-grid4-ec2.yml down -d"
 echo "  --> Remove docker-compose-grid4-ec2.yml file"
 echo "  --> Stop EC2 Instance"
-aws ec2 stop-instances --instance-ids i-0456c30d3ffd6aad3
+region='ap-south-1'
+aws ec2 stop-instances --instance-ids i-0456c30d3ffd6aad3 --region $region
